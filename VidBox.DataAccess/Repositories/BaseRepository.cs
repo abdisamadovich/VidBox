6a2abbbaf0ab1ca5e73518b4da51edcee6364f6c
abdisamadovich <islombobokulov143@gmail.com>
2023-09-20 20:32:23 +0500

    db

--- VidBox.DataAccess/Repositories/BaseRepository.cs
+++ VidBox.DataAccess/Repositories/BaseRepository.cs
@@ -11,7 +11,7 @@ namespace VidBox.DataAccess.Repositories
         {
             SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
             Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
-            this._connection = new NpgsqlConnection("Host=vidbox-do-user-14588306-0.b.db.ondigitalocean.com; Port=25060; Database=defaultdb; User Id=doadmin; Password=AVNS_y7zTXGDeMDXMzaLeEX2;");
+            this._connection = new NpgsqlConnection("Host=vidbox-do-user-14588306-0.b.db.ondigitalocean.com; Port=25060; Database=vidbox; User Id=doadmin; Password=AVNS_y7zTXGDeMDXMzaLeEX2;");
         }
     }
 }
